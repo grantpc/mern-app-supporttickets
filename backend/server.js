f71c4d406348187e9818d98daee1acc2e9e141bb
Grant Christensen <59584917+grantpc@users.noreply.github.com>
2022-06-10 12:43:54 -0600

    Prepare to deploy

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config();
 const colors = require('colors');
 const errorHandler = require('./middleware/errorHandler');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -20,6 +21,21 @@ const tickets = require('./routes/ticketRoutes');
 // Mounting routes
 app.use('/api/v1/users', users);
 app.use('/api/v1/tickets', tickets);
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  // Load the index.html file in that build folder
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html');
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' });
+  });
+}
 
 // Error handler middleware
 app.use(errorHandler);
